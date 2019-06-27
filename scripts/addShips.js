a82b3a53071acadee0d139ab5ad0081ae325424e
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-06-27 22:02:33 +0200

    Fix ship automatic ship placement

--- scripts/addShips.js
+++ scripts/addShips.js
@@ -18,7 +18,6 @@ function placesShips() {
     let shipsArrayNames = Object.keys(ships);
     shipsArrayNames.pop();
 
-
     for (let i = 0; i < numbersOfShips; i++) {
         let isShipPlaced = false;
         while (!isShipPlaced) {
@@ -37,6 +36,7 @@ function placesShips() {
                         coordinates.x += 1;
                     }
                 }
+
             }
         }
     }
@@ -47,13 +47,12 @@ function testShipPlacement(coordinates, shipLength, direction) {
         let EXTRA_SQUARES = 2;
         let totalLinesToCheck = getLinesToCheck(direction, shipLength, EXTRA_SQUARES);
         let squaresInLineToCheck = getSquaresToCheck(direction, shipLength, EXTRA_SQUARES);
-        let y = coordinates.x;
-        let x = coordinates.y;
+        let y = coordinates.x - 1;
+        let x = coordinates.y - 1;
         for (let startY = 0; startY < totalLinesToCheck; startY++) {
             for (let startX = 0; startX < squaresInLineToCheck; startX++) {
-                if (0 <= y && y <10 && 0 <= x && x < 10) {
-                    if (shipsGrid[`cell_${y}${x}`] === "ocean") {
-                        // or StartY, StartX
+                if (y >= 0 && y < 10 && x >= 0 && x < 10) {
+                    if (shipsGrid[`cell_${y}${x}`].contain !== "ocean") {
                         return false;
                     }
                 }
@@ -93,56 +92,4 @@ function checkIfShipInMap(coordinates, length, direction) {
     } else {
         return false;
     }
-}
-
-
-function addShips() {
-
-
-
-    // console.log(getRandomNumber());
-
-    // //carrier
-    // shipsGrid["cell_00"].contain = 'carrier';
-    // document.querySelector('#cell_00').classList.add('carrier');
-    // shipsGrid["cell_10"].contain = 'carrier';
-    // document.querySelector('#cell_10').classList.add('carrier');
-    // shipsGrid["cell_20"].contain = 'carrier';
-    // document.querySelector('#cell_20').classList.add('carrier');
-    // shipsGrid["cell_30"].contain = 'carrier';
-    // document.querySelector('#cell_30').classList.add('carrier');
-    // shipsGrid["cell_40"].contain = 'carrier';
-    // document.querySelector('#cell_40').classList.add('carrier');
-    //
-    // //battleship
-    // shipsGrid["cell_24"].contain = 'battleship';
-    // document.querySelector('#cell_24').classList.add('battleship');
-    // shipsGrid["cell_25"].contain = 'battleship';
-    // document.querySelector('#cell_25').classList.add('battleship');
-    // shipsGrid["cell_26"].contain = 'battleship';
-    // document.querySelector('#cell_26').classList.add('battleship');
-    // shipsGrid["cell_27"].contain = 'battleship';
-    // document.querySelector('#cell_27').classList.add('battleship');
-    //
-    // //cruiser
-    // shipsGrid["cell_62"].contain = 'cruiser';
-    // document.querySelector('#cell_62').classList.add('cruiser');
-    // shipsGrid["cell_72"].contain = 'cruiser';
-    // document.querySelector('#cell_72').classList.add('cruiser');
-    // shipsGrid["cell_82"].contain = 'cruiser';
-    // document.querySelector('#cell_82').classList.add('cruiser');
-    //
-    // //submarine
-    // shipsGrid["cell_56"].contain = 'submarine';
-    // document.querySelector('#cell_56').classList.add('submarine');
-    // shipsGrid["cell_57"].contain = 'submarine';
-    // document.querySelector('#cell_57').classList.add('submarine');
-    // shipsGrid["cell_58"].contain = 'submarine';
-    // document.querySelector('#cell_58').classList.add('submarine');
-    //
-    // //destroyer
-    // shipsGrid["cell_89"].contain = 'destroyer';
-    // document.querySelector('#cell_89').classList.add('destroyer');
-    // shipsGrid["cell_99"].contain = 'destroyer';
-    // document.querySelector('#cell_99').classList.add('destroyer');
 }
