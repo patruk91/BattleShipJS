ed5e86d370ca28a87c305e1a6ca846c95f403310
Krzysiek <krzysiekczaja@gmail.com>
2019-06-27 15:53:21 +0200

    Extract function to handle player join existing game

--- scripts/main.js
+++ scripts/main.js
@@ -43,7 +43,7 @@ function logInUser() {
     });
 }
 
-function handleConnectionWithNoGameOpen(snapshot) {
+function handleConnectionWithNoGameOpen(snapshot, loaderH2, loaderSpin) {
     dbRef.collection('games').doc(snapshot.docs[0].id).update({
         player2: userId,
         status: 'close'
@@ -58,8 +58,51 @@ function handleConnectionWithNoGameOpen(snapshot) {
                 let listener = dbRef.collection('games').doc(gameId).onSnapshot(function(doc) {
                     if(doc.data().status === 'close') {
                         console.log('Start Game');
-                        // loaderSpin.style.display = "none";
-                        loaderH2.innerHTML = "";
+                        document.querySelector("#loaderh2").innerHTML = "";
+                        startGame(listener());
+                    }
+                });
+            }
+        });
+    });
+}
+
+function matchmaking() {
+    dbRef.collection('games').where('status', '==', 'open').get().then(snapshot => {
+        if (snapshot.docs.length !== 0) {
+            handleConnectionWithNoGameOpen(snapshot);
+        }
+        else {
+            handleConnectionToExistingGame();
+        }
+    });
+}
+
+function handleConnectionToExistingGame(loaderSpin) {
+    displayPopUp();
+    document.querySelector("#loader").className = "loader";
+    dbRef.collection('games').add({
+        player1: userId,
+        player2: '',
+        sequence: userId,
+        phase: 'shoot',
+        coordinates: '',
+        shootGrid: '',
+        gameEnd: false,
+        status: 'open'
+    }).then(docRef => {
+        dbRef.collection('users').doc(userId).update({
+            inGame: docRef.id
+        });
+    }).then(() => {
+        dbRef.collection('users').doc(userId).get().then(gameName => {
+            gameId = gameName.data().inGame;
+            if (gameId !== "") {
+                let listener = dbRef.collection('games').doc(gameId).onSnapshot(function(doc) {
+                    if(doc.data().status === 'close') {
+                        document.querySelector("#loaderh2").innerHTML = "";
+                        console.log('Start Game');
+                        closePopUp();
                         startGame(listener());
                     }
                 });
@@ -69,47 +112,9 @@ function handleConnectionWithNoGameOpen(snapshot) {
 }
 
 function main() {
-    const loaderSpin = document.querySelector("#loader");
-    const loaderH2 = document.querySelector("#loaderh2");
-
     logInUser();
 
-    dbRef.collection('games').where('status', '==', 'open').get().then(snapshot => {
-        if(snapshot.docs.length !== 0) {
-            handleConnectionWithNoGameOpen(snapshot);
-        } else {
-            displayPopUp();
-            loaderSpin.className = "loader";
-            dbRef.collection('games').add({
-                player1: userId,
-                player2: '',
-                sequence: userId,
-                phase: 'shoot',
-                coordinates: '',
-                shootGrid: '',
-                gameEnd: false,
-                status: 'open'
-            }).then(docRef => {
-                dbRef.collection('users').doc(userId).update({
-                    inGame: docRef.id
-                });
-            }).then(() => {
-                dbRef.collection('users').doc(userId).get().then(gameName => {
-                    gameId = gameName.data().inGame;
-                    if (gameId !== "") {
-                        let listener = dbRef.collection('games').doc(gameId).onSnapshot(function(doc) {
-                            if(doc.data().status === 'close') {
-                                loaderH2.innerHTML = "";
-                                console.log('Start Game');
-                                closePopUp();
-                                startGame(listener());
-                            }
-                        });
-                    }
-                });
-            });
-        }
-    })
+    matchmaking();
 }
 
 main();
