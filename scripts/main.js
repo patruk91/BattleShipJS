87e082982595696d574f98c87a5e39536d75a7b6
Krzysiek <krzysiekczaja@gmail.com>
2019-06-27 16:21:39 +0200

    Move matchmaking and firebase config to separate files

--- scripts/main.js
+++ scripts/main.js
@@ -29,87 +29,84 @@ let ships = {
     }
 };
 
-function logInUser() {
-    window.addEventListener("beforeunload", e => {
-        firebase.auth().signInAnonymously();
-    });
+// function logInUser() {
+//     window.addEventListener("beforeunload", e => {
+//         firebase.auth().signInAnonymously();
+//     });
 
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-        userId = firebaseUser.uid;
+//     firebase.auth().onAuthStateChanged(firebaseUser => {
+//         userId = firebaseUser.uid;
 
-        dbRef.collection('users').doc(userId).set({
-            inGame: "",
-        })
-    });
-}
+//         dbRef.collection('users').doc(userId).set({
+//             inGame: "",
+//         })
+//     });
+// }
 
-function handleConnectionWithNoGameOpen(snapshot, loaderH2, loaderSpin) {
-    dbRef.collection('games').doc(snapshot.docs[0].id).update({
-        player2: userId,
-        status: 'close'
-    }).then(() => {
-        dbRef.collection('users').doc(userId).update({
-            inGame: snapshot.docs[0].id
-        });
-    }).then(() => {
-        dbRef.collection('users').doc(userId).get().then(gameName => {
-            gameId = gameName.data().inGame;
-            if (gameId !== "") {
-                let listener = dbRef.collection('games').doc(gameId).onSnapshot(function(doc) {
-                    if(doc.data().status === 'close') {
-                        console.log('Start Game');
-                        document.querySelector("#loaderh2").innerHTML = "";
-                        startGame(listener());
-                    }
-                });
-            }
-        });
-    });
-}
+// function endMatchmaking() {
+//     let listener = dbRef.collection('games').doc(gameId).onSnapshot(function(doc) {
+//         if(doc.data().status === 'close') {
+//             console.log('Start Game');
+//             document.querySelector("#loaderh2").innerHTML = "";
+//             startGame(listener());
+//         }
+//     });
+// }
 
-function matchmaking() {
-    dbRef.collection('games').where('status', '==', 'open').get().then(snapshot => {
-        if (snapshot.docs.length !== 0) {
-            handleConnectionWithNoGameOpen(snapshot);
-        }
-        else {
-            handleConnectionToExistingGame();
-        }
-    });
-}
+// function handleConnectionWithNoGameOpen(snapshot, loaderH2, loaderSpin) {
+//     dbRef.collection('games').doc(snapshot.docs[0].id).update({
+//         player2: userId,
+//         status: 'close'
+//     }).then(() => {
+//         dbRef.collection('users').doc(userId).update({
+//             inGame: snapshot.docs[0].id
+//         });
+//     }).then(() => {
+//         dbRef.collection('users').doc(userId).get().then(gameName => {
+//             gameId = gameName.data().inGame;
+//             if (gameId !== "") {
+//                 endMatchmaking();
+//             }
+//         });
+//     });
+// }
 
-function handleConnectionToExistingGame(loaderSpin) {
-    displayPopUp();
-    document.querySelector("#loader").className = "loader";
-    dbRef.collection('games').add({
-        player1: userId,
-        player2: '',
-        sequence: userId,
-        phase: 'shoot',
-        coordinates: '',
-        shootGrid: '',
-        gameEnd: false,
-        status: 'open'
-    }).then(docRef => {
-        dbRef.collection('users').doc(userId).update({
-            inGame: docRef.id
-        });
-    }).then(() => {
-        dbRef.collection('users').doc(userId).get().then(gameName => {
-            gameId = gameName.data().inGame;
-            if (gameId !== "") {
-                let listener = dbRef.collection('games').doc(gameId).onSnapshot(function(doc) {
-                    if(doc.data().status === 'close') {
-                        document.querySelector("#loaderh2").innerHTML = "";
-                        console.log('Start Game');
-                        closePopUp();
-                        startGame(listener());
-                    }
-                });
-            }
-        });
-    });
-}
+// function handleConnectionToExistingGame(loaderSpin) {
+//     displayPopUp();
+//     document.querySelector("#loader").className = "loader";
+//     dbRef.collection('games').add({
+//         player1: userId,
+//         player2: '',
+//         sequence: userId,
+//         phase: 'shoot',
+//         coordinates: '',
+//         shootGrid: '',
+//         gameEnd: false,
+//         status: 'open'
+//     }).then(docRef => {
+//         dbRef.collection('users').doc(userId).update({
+//             inGame: docRef.id
+//         });
+//     }).then(() => {
+//         dbRef.collection('users').doc(userId).get().then(gameName => {
+//             gameId = gameName.data().inGame;
+//             if (gameId !== "") {
+//                 endMatchmaking();
+//             }
+//         });
+//     });
+// }
+
+// function matchmaking() {
+//     dbRef.collection('games').where('status', '==', 'open').get().then(snapshot => {
+//         if (snapshot.docs.length !== 0) {
+//             handleConnectionWithNoGameOpen(snapshot);
+//         }
+//         else {
+//             handleConnectionToExistingGame();
+//         }
+//     });
+// }
 
 function main() {
     logInUser();
